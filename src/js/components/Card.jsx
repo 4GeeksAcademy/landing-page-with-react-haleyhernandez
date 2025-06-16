8c7cf023f27fc13331805b5520151c18194a98ba
Haley <haleymariehernandez10@gmail.com>
2025-06-16 21:03:16 +0000

    third push

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import flower from "../../img/flower.jpg"
 
 //create your first component
 const Card = (props) => {
     return (
             <div className="card" style={{ width: "18rem" }}>
-                <img className="card-img-top" src=".../500px325/" alt="Card image cap" />
+                <img className="card-img-top" src= {flower} alt="Card image cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">{props.text}</p>
---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//create your first component
+const Footer = (props) => {
+    return (
+            <nav class="col-6 navbar bg-dark w-50 mx-auto">
+  <div class="container-fluid w-50 mx-auto">
+    <span class="navbar-brand mb-0 h1 fs-6 w-50 mx-auto bg-dark text-white">Haley website 2025</span>
+  </div>
+</nav>
+    );
+};
+
+export default Footer;
